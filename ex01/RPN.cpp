c5c4a5708b146a8fcaaeed264456076a36c32912
Hamza Boussetta <hbousset@c2r1p12.1337.ma>
2025-11-20 22:06:53 +0100

    Add Reverse Polish Notation (RPN) evaluator implementation and supporting files

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,13 +6,74 @@
 /*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 14:25:55 by hbousset          #+#    #+#             */
-/*   Updated: 2025/11/19 14:27:22 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/11/20 22:05:29 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
-int	main(int ac, char **av)
+static void performOperation(std::list<int>& stack, char op)
 {
-	
+	if (stack.size() < 2)
+		throw std::runtime_error("error: something went wrong in your equation twin!");
+
+	int b = stack.back();
+	stack.pop_back();
+	int a = stack.back();
+	stack.pop_back();
+
+	int result;
+	switch (op)
+	{
+		case '+':
+			result = a + b;
+			break;
+		case '-':
+			result = a - b;
+			break;
+		case '*':
+			result = a * b;
+			break;
+		case '/':
+			if (b == 0)
+				throw std::runtime_error("error: what did you thought when you were dividing by 0?");
+			result = a / b;
+			break;
+		default:
+			throw std::runtime_error("error: i mean get smarter or whatever");
+	}
+	stack.push_back(result);
+}
+
+void	RPN(std::string input)
+{
+	std::list<int> stack;
+
+	for (size_t i = 0; i < input.length(); i++)
+	{
+		char c = input[i];
+
+		if (c == ' ')
+			continue;
+
+		if (isdigit(c))
+		{
+			if (i + 1 < input.length() && isdigit(input[i + 1]))
+				throw std::runtime_error("error: problem in formating the equation!");
+			stack.push_back(c - '0');
+		}
+		else if (c == '+' || c == '-' || c == '*' || c == '/')
+		{
+			performOperation(stack, c);
+		}
+		else
+		{
+			throw std::runtime_error("error: expected an operator ngl twin!");
+		}
+	}
+
+	if (stack.size() != 1)
+		throw std::runtime_error("error: invalid equation ngl twin!");
+
+	std::cout << stack.back() << std::endl;
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,19 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/19 14:26:04 by hbousset          #+#    #+#             */
-/*   Updated: 2025/11/20 21:56:06 by hbousset         ###   ########.fr       */
+/*   Created: 2025/11/20 21:43:39 by hbousset          #+#    #+#             */
+/*   Updated: 2025/11/20 22:01:57 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#include "RPN.hpp"
 
-#include <iostream>
-#include <list>
-#include <stdexcept>
-
-void	RPN(std::string input);
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		std::cerr << "error: incorrect number of argument!" << std::endl;
+		return 1;
+	}
+	std::string input = av[1];
+	try
+	{
+		RPN(input);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
